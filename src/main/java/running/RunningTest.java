278de7255e5f1216f8b7c0d1a00d8c08fa53c6b0
Been980804 <dlgusqls980804@naver.com>
2024-08-01 12:21:08 +0900

    fix: runningTest

--- src/main/java/running/RunningTest.java
+++ src/main/java/running/RunningTest.java
@@ -1,9 +1,8 @@
 package running;
 
-import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 
 import org.junit.Test;
 
@@ -14,47 +13,59 @@ import util.DBUtil;
 
 public class RunningTest {
 
-	@Test
-	public void step01Test() {
-		EntityManager em = null;
-		EntityTransaction tx = null;
+//	@Test
+	public void AllMember() {
+		EntityManager em = DBUtil.getEntityManager();
+
+		List<Member> members = em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
 		
-		try {
-			em = DBUtil.getEntityManager();
-			tx = em.getTransaction();
-			tx.begin();
-			
-			Member m1 = new Member("이승준", "abc@gmail.com");
-			Member m2 = new Member("이현빈", "def@gmail.com");
-			Member m3 = new Member("조성현", "ghi@gmail.com");
-			Member m4 = new Member("허예은", "jkl@gmail.com");
-			em.persist(m1);
-			em.persist(m2);
-			em.persist(m3);
-			em.persist(m4);
-			
-			Product p1 = new Product(m1, "휴지", 16000);
-			Product p2 = new Product(m2, "생수", 12000);
-			em.persist(p1);
-			em.persist(p2);
-			
-			Subscribe s1 = new Subscribe(m1, p1, 20, 50, 20240730);
-			Subscribe s2 = new Subscribe(m2, p2, 10, 60, 20240728);
-			Subscribe s3 = new Subscribe(m3, p1, 50, 70, 20240710);
-			em.persist(s1);
-			em.persist(s2);
-			em.persist(s3);
-			
-			tx.commit();
-			
-		}catch(Exception e) {
-			tx.rollback();
-			e.printStackTrace();
-		}finally {
-			if(em != null) {
-				em.close();
-				em = null;
-			}
+		for(Member m : members) {
+			System.out.println(m);
 		}
+		
+		em.close();
+		em = null;
+	}
+	
+//	@Test
+	public void AllProduct() {
+		EntityManager em = DBUtil.getEntityManager();
+
+		List<Product> products = em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
+		
+		for(Product p : products) {
+			System.out.println(p);
+		}
+		
+		em.close();
+		em = null;
+	}
+	
+//	@Test
+	public void AllSubscribe() {
+		EntityManager em = DBUtil.getEntityManager();
+
+		List<Subscribe> subscribes = em.createQuery("SELECT s FROM Subscribe s", Subscribe.class).getResultList();
+		
+		for(Subscribe s : subscribes) {
+			System.out.println(s);
+		}
+		
+		em.close();
+		em = null;
+	}
+	
+	@Test
+	public void findTest() {
+		EntityManager em = DBUtil.getEntityManager();
+
+		Subscribe s = em.find(Subscribe.class, 2);
+		System.out.println(s.getSubId() + " " + 
+						   s.getMemId().getMemName() + " " + 
+						   s.getProdId().getProdName() + " " + 
+						   s.getProdId().getProdPrice());
+		
+		em.close();
+		em = null;
 	}
 }
